680a4dbd35b466f5f9eedef28a18eef461493761
Sebastien Brumbaugh <66973931+loknarb@users.noreply.github.com>
2022-06-25 01:03:20 +0200

    datecard init

---
+++ src/components/DateCard.vue
@@ -0,0 +1,14 @@
+<template>
+  <h1>{{ date }}</h1>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "DateCard",
+  props: {
+    date: String,
+  },
+});
+</script>
