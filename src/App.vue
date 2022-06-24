3a20f8b9f965a48497190c3a0845d84a45aaa8de
Sebastien Brumbaugh <66973931+loknarb@users.noreply.github.com>
2022-06-25 00:21:16 +0200

    changed default tempalte to main

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <template>
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  <Main msg="Welcome to Your Vue.js + TypeScript App" />
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import HelloWorld from "./components/HelloWorld.vue";
+import Main from "./components/Main.vue";
 
 export default defineComponent({
   name: "App",
   components: {
-    HelloWorld,
+    Main,
   },
 });
 </script>
