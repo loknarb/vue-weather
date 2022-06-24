0505821da851b9ad2fb053f92b8458fff8ab557f
Sebastien Brumbaugh <66973931+loknarb@users.noreply.github.com>
2022-06-25 01:00:23 +0200

    created datecard logic updated app and passthru

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,45 @@
 <template>
   <Main msg="Welcome to Your Vue.js + TypeScript App" />
+  <div v-for="date in dateDayLong" :key="date">
+    <DateCard :date="date" />
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
 import Main from "./components/Main.vue";
-
+import DateCard from "./components/DateCard.vue";
 export default defineComponent({
   name: "App",
   components: {
     Main,
+    DateCard,
+  },
+  data: () => ({
+    dateDayLong: [] as string[],
+  }),
+  methods: {
+    getDateDayLong() {
+      let x = [] as string[];
+      const date = new Date();
+      const day = date.getDay();
+      const dayLong = [
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday",
+      ];
+      for (let i = 0; i < 5; i++) {
+        x.push(dayLong[day + i]);
+      }
+      return x;
+    },
+  },
+  mounted: function () {
+    this.dateDayLong = this.getDateDayLong();
   },
 });
 </script>
