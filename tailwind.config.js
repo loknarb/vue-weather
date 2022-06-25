d38d2720241273d10bfb039760ddbbcbcf561f33
Sebastien Brumbaugh <66973931+loknarb@users.noreply.github.com>
2022-06-25 13:42:51 +0200

    added daisy and imported

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,26 @@
 module.exports = {
+  plugins: [require("daisyui")],
   content: [
     "./public/index.html",
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {}
+  },
+  daisyui: {
+    themes: [
+      {
+        light: {
+          ...require("daisyui/src/colors/themes")["[data-theme]/light"],
+          primary: "#e2ece9",
+          secondary: "#fde2e4",
+          accent: 'cddafd', // or #dfe7fd
+          "base-100": "#f0efeb" // or  #fff1e6
+
+
+        }
+      }
+    ]
   },
   plugins: [],
 }
