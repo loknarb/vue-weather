b105e33fd75b9b97f773ed8724197c6f5587340b
Sebastien Brumbaugh <66973931+loknarb@users.noreply.github.com>
2022-06-25 14:39:11 +0200

    adjusted daisy

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,15 +12,12 @@ module.exports = {
       {
         light: {
           ...require("daisyui/src/colors/themes")["[data-theme]/light"],
-          primary: "#e2ece9",
-          secondary: "#fde2e4",
-          accent: 'cddafd', // or #dfe7fd
+          "primary": "#e2ece9",
+          "secondary": "#fde2e4",
+          "accent": "#cddafd", // or #dfe7fd
           "base-100": "#f0efeb" // or  #fff1e6
-
-
         }
       }
     ]
   },
-  plugins: [],
 }
