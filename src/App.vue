6a9e5ef210bae71b09f2f1f85558e04d29f2926b
Sebastien Brumbaugh <66973931+loknarb@users.noreply.github.com>
2022-06-27 11:50:38 +0200

    creation of date separation of api data

--- src/App.vue
+++ src/App.vue
@@ -23,7 +23,9 @@ export default defineComponent({
     getDateDayLong() {
       let x = [] as string[];
       const date = new Date();
+      const CURR_DAY_EPOCH = Math.floor(date.getTime() / 1000);
       const day = date.getDay();
+      const DAY_DELTA = 86400;
 
       const dayLong = [
         "Sunday",
@@ -34,13 +36,34 @@ export default defineComponent({
         "Friday",
         "Saturday",
       ];
+      let i = 0;
+      forecast.list.forEach((dayObject) => {
+        // console.log(dayObject.main.temp);
+        if (CURR_DAY_EPOCH < dayObject.dt && Number.isInteger(dayObject.dt / DAY_DELTA)) {
+          console.log(dayObject.dt - CURR_DAY_EPOCH); // this will be new date
+          // console.log(new Date(dayObject.dt * 1000));
+        } else if (CURR_DAY_EPOCH < dayObject.dt) {
+          // this will be iterations in 3h intervals of curr day
+          console.log(new Date(dayObject.dt * 1000));
+          console.log("not integer");
+        } else {
+          // nothing interesting just past dates this shouldn't happen when we start pulling api data
+          return;
+        }
+      });
       for (let i = 0; i < 5; i++) {
+        // 5 days forecast
         if (day + i > 6) {
+          // this is should reset to 0 when it hits above 6 implying it is sunday
+          // ex Monday
           // todo add our json file looping through list and pushing about 8 for days past
 
           x[i] = dayLong[day + i - 7];
+          console.log(x[i]);
         } else {
+          // this should return the day if it is less than 6
           x[i] = dayLong[day + i];
+          console.log(x[i]);
         }
       }
       return x;
