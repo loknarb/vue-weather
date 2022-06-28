0d3b33627ed7bc46361010d998f5feb504283aff
loknarbLaptop <66973931+loknarb@users.noreply.github.com>
2022-06-28 14:05:15 +0200

    object created duh..

--- src/App.vue
+++ src/App.vue
@@ -10,6 +10,7 @@ import { defineComponent } from "vue";
 import Main from "./components/Main.vue";
 import DateCard from "./components/DateCard.vue";
 import forecast from "../public/forecast.json";
+import { weatherOptions } from "../src/components/types";
 export default defineComponent({
   name: "App",
   components: {
@@ -17,11 +18,11 @@ export default defineComponent({
     DateCard,
   },
   data: () => ({
-    dateDayLong: [] as any[],
+    dateDayLong: {} as weatherOptions,
   }),
   methods: {
     getDateDayLong() {
-      let x = [] as any[]; // this needs to be more specifically typed
+      let x: weatherOptions = {}; // this needs to be more specifically typed
       const date = new Date();
       const CURR_DAY_EPOCH = Math.floor(date.getTime() / 1000);
       const day = date.getDay();
@@ -36,51 +37,56 @@ export default defineComponent({
         "Friday",
         "Saturday",
       ];
-      let k = 0;
       for (let i = 0; i < 5; i++) {
         if (day + i > 6) {
-          x.push({ [dayLong[day + i - 7]]: {} });
+          x[`${dayLong[day + i - 7]}`] = [];
         } else {
-          x.push({ [dayLong[day + i]]: {} });
+          x[`${dayLong[day + i]}`] = [];
         }
-        // this is not ideal we are recursively calling this function 5 times
-        forecast.list.forEach((dayObject) => {
-          // console.log(dayObject.main.temp);
-          if (CURR_DAY_EPOCH < dayObject.dt && Number.isInteger(dayObject.dt / DAY_DELTA)) {
-            // this will be new date
-            console.log(k);
-            if (k > 5) {
-              return;
-            } else {
-              k++;
-              console.log("new day", x[k]);
-            }
-
-            // x[k][dayLong[day + i - 7]] = {
-            //   temp: dayObject.main.temp,
-            //   icon: dayObject.weather[0].icon,
-            //   description: dayObject.weather[0].description,
-            // };
-
-            // todo add our json file looping through list and pushing about 8 for days past
-          } else if (CURR_DAY_EPOCH < dayObject.dt) {
-            // this will be iterations in 3h intervals of curr day
-            if (k > 5) {
-              return;
-            } else {
-              console.log("curr day", x[k]);
-            }
-            // x[k][dayLong[day + i]] = {
-            //   temp: dayObject.main.temp,
-            //   icon: dayObject.weather[0].icon,
-            //   description: dayObject.weather[0].description,
-            // };
-          } else {
-            // nothing interesting just past dates this shouldn't happen when we start pulling api data
-            return;
-          }
-        });
       }
+      forecast.list.forEach((dayObject) => {
+        // console.log(dayObject.main.temp);
+
+        if (CURR_DAY_EPOCH < dayObject.dt && Number.isInteger(dayObject.dt / DAY_DELTA)) {
+          // this will be new date
+          // x[i][dayLong[day + i - 7]].push(dayObject);
+          // const forecastDateDay = new Date(dayObject.dt * 1000);
+          const forecastDayString = new Date(dayObject.dt * 1000).toLocaleDateString("en-US", {
+            weekday: "long",
+          });
+          console.log("x", x);
+          console.log("STRING ", forecastDayString);
+          // console.log(x[forecastDayString]);
+          // new Intl.DateTimeFormat('en-US', {weekday: 'long'}).format(forecastDateDay)
+          // x[forecastDateDay].push(dayObject);
+          // console.log("new day", x[i]);
+          // console.log("day", day);
+          // console.log("index", i);
+          // console.log("daylongDay", dayLong[day + i - 7]);
+
+          // x[k][dayLong[day + i - 7]] = {
+          //   temp: dayObject.main.temp,
+          //   icon: dayObject.weather[0].icon,
+          //   description: dayObject.weather[0].description,
+          // };
+
+          // todo add our json file looping through list and pushing about 8 for days past
+        } else if (CURR_DAY_EPOCH < dayObject.dt) {
+          // this will be iterations in 3h intervals of curr day
+          const forecastDateDay = new Date(dayObject.dt * 1000).getDay();
+          // x[forecastDateDay].push(dayObject);
+          // x[i][dayLong[day + i]].push(dayObject);
+          // x[k][dayLong[day + i]] = {
+          //   temp: dayObject.main.temp,
+          //   icon: dayObject.weather[0].icon,
+          //   description: dayObject.weather[0].description,
+          // };
+        } else {
+          // nothing interesting just past dates this shouldn't happen when we start pulling api data
+          return;
+        }
+      });
+
       console.log(x);
       return x;
     },
