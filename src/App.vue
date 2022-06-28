378702e7963bb52b9eadac9f6513c15720afd784
loknarbLaptop <66973931+loknarb@users.noreply.github.com>
2022-06-28 15:12:17 +0200

    finished basic algorithm for api call

--- src/App.vue
+++ src/App.vue
@@ -10,7 +10,12 @@ import { defineComponent } from "vue";
 import Main from "./components/Main.vue";
 import DateCard from "./components/DateCard.vue";
 import forecast from "../public/forecast.json";
-import { weatherOptions } from "../src/components/types";
+import {
+  weatherOptions,
+  weatherTemperature,
+  weatherDescription,
+  weatherObject,
+} from "../src/components/types";
 export default defineComponent({
   name: "App",
   components: {
@@ -18,11 +23,11 @@ export default defineComponent({
     DateCard,
   },
   data: () => ({
-    dateDayLong: {} as weatherOptions,
+    dateDayLong: {} as weatherObject,
   }),
   methods: {
     getDateDayLong() {
-      let x: weatherOptions = {}; // this needs to be more specifically typed
+      let weatherObject: weatherObject = {}; // this needs to be more specifically typed
       const date = new Date();
       const CURR_DAY_EPOCH = Math.floor(date.getTime() / 1000);
       const day = date.getDay();
@@ -39,56 +44,36 @@ export default defineComponent({
       ];
       for (let i = 0; i < 5; i++) {
         if (day + i > 6) {
-          x[`${dayLong[day + i - 7]}`] = [];
+          weatherObject[`${dayLong[day + i - 7]}`] = [];
         } else {
-          x[`${dayLong[day + i]}`] = [];
+          weatherObject[`${dayLong[day + i]}`] = [];
         }
       }
       forecast.list.forEach((dayObject) => {
-        // console.log(dayObject.main.temp);
-
-        if (CURR_DAY_EPOCH < dayObject.dt && Number.isInteger(dayObject.dt / DAY_DELTA)) {
-          // this will be new date
-          // x[i][dayLong[day + i - 7]].push(dayObject);
-          // const forecastDateDay = new Date(dayObject.dt * 1000);
+        if (dayObject.dt - CURR_DAY_EPOCH > 0) {
+          // this will be our current day and the next 5 days
+          const weatherTemperature: weatherTemperature = {
+            temp: dayObject.main.temp,
+          };
+          const weatherDescription: weatherDescription = {
+            description: dayObject.weather[0].description,
+            icon: dayObject.weather[0].icon,
+            id: dayObject.weather[0].id,
+          };
+          const weatherOptions: weatherOptions = {
+            dt: dayObject.dt,
+            main: weatherTemperature,
+            weather: weatherDescription,
+          };
+          // we need to pull the day from the date object and compare it to our weatherObject
           const forecastDayString = new Date(dayObject.dt * 1000).toLocaleDateString("en-US", {
             weekday: "long",
           });
-          console.log("x", x);
-          console.log("STRING ", forecastDayString);
-          // console.log(x[forecastDayString]);
-          // new Intl.DateTimeFormat('en-US', {weekday: 'long'}).format(forecastDateDay)
-          // x[forecastDateDay].push(dayObject);
-          // console.log("new day", x[i]);
-          // console.log("day", day);
-          // console.log("index", i);
-          // console.log("daylongDay", dayLong[day + i - 7]);
-
-          // x[k][dayLong[day + i - 7]] = {
-          //   temp: dayObject.main.temp,
-          //   icon: dayObject.weather[0].icon,
-          //   description: dayObject.weather[0].description,
-          // };
-
-          // todo add our json file looping through list and pushing about 8 for days past
-        } else if (CURR_DAY_EPOCH < dayObject.dt) {
-          // this will be iterations in 3h intervals of curr day
-          const forecastDateDay = new Date(dayObject.dt * 1000).getDay();
-          // x[forecastDateDay].push(dayObject);
-          // x[i][dayLong[day + i]].push(dayObject);
-          // x[k][dayLong[day + i]] = {
-          //   temp: dayObject.main.temp,
-          //   icon: dayObject.weather[0].icon,
-          //   description: dayObject.weather[0].description,
-          // };
-        } else {
-          // nothing interesting just past dates this shouldn't happen when we start pulling api data
-          return;
+          weatherObject[forecastDayString].push(weatherOptions);
         }
       });
-
-      console.log(x);
-      return x;
+      console.log(weatherObject);
+      return weatherObject;
     },
   },
   mounted: function () {
