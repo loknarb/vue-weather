e2ef418dca83513b6261a0154ce652fbc5b26689
loknarbLaptop <66973931+loknarb@users.noreply.github.com>
2022-06-28 16:01:51 +0200

    added dirty string

--- src/components/DateCard.vue
+++ src/components/DateCard.vue
@@ -2,20 +2,23 @@
   <div class="w-56 mx-auto my-4 shadow-xl bg-primary-focus lg:mx-4 card">
     <div class="card-body">
       <div class="card bg-primary">
-        <figure><img src="../assets/04d@2x.png" /></figure>
+        <figure><img :src="`../assets/${date[0].weather.icon}.png`" /></figure>
       </div>
-      <h1 class="justify-center text-secondary card-title">{{ date }}</h1>
+      <h1 class="justify-center text-secondary card-title">{{ date[0].main.temp }}</h1>
     </div>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
-
+import { defineComponent, PropType } from "vue";
+import { weatherObject } from "./types";
 export default defineComponent({
   name: "DateCard",
   props: {
-    date: String,
+    date: {
+      type: Object as PropType<weatherObject>,
+      required: true,
+    },
   },
 });
 </script>
