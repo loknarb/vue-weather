fc9672793aa02788da28fb948d09176c5b91e65e
Sebastien Brumbaugh <66973931+loknarb@users.noreply.github.com>
2022-07-04 16:11:20 +0200

    removed top logic of label instead to be held by a div element

--- src/components/DateCard.vue
+++ src/components/DateCard.vue
@@ -1,24 +1,29 @@
 <template>
-  <div class="w-56 mx-auto my-4 shadow-xl bg-primary-focus lg:mx-4 card">
-    <div class="card-body">
-      <div class="card bg-primary-focus opacity-90">
-        <figure><img :src="require(`../assets/${date[0].weather.icon}@2x.png`)" /></figure>
-      </div>
-      <h1 class="justify-center text-5xl text-secondary card-title">
-        {{ avgTemp }}
-      </h1>
-      <h1 class="justify-center text-3xl text-secondary card-title">{{ dayOfWeek }}</h1>
-      <div class="flex justify-between">
-        <div>
-          <span>HI:</span>
-          <h3 class="justify-center text-secondary-focus opacity-80 card-title">{{ maxTemp }}</h3>
+  <div
+    class="w-56 mx-auto my-4 shadow-xl bg-primary-focus lg:mx-4 card hover:scale-105 transition-all"
+    @click="selectDate"
+  >
+    <label for="my-modal" class="cursor-pointer">
+      <div class="card-body">
+        <div class="card bg-primary-focus opacity-90">
+          <figure><img :src="require(`../assets/${date[0].weather.icon}@2x.png`)" /></figure>
         </div>
-        <div>
-          <span>LO:</span>
-          <h3 class="justify-center text-secondary-focus opacity-80 card-title">{{ minTemp }}</h3>
+        <h1 class="justify-center text-5xl text-secondary card-title">
+          {{ avgTemp }}
+        </h1>
+        <h1 class="justify-center text-3xl text-secondary card-title">{{ dayOfWeek }}</h1>
+        <div class="flex justify-between">
+          <div>
+            <span>HI:</span>
+            <h3 class="justify-center text-secondary-focus opacity-80 card-title">{{ maxTemp }}</h3>
+          </div>
+          <div>
+            <span>LO:</span>
+            <h3 class="justify-center text-secondary-focus opacity-80 card-title">{{ minTemp }}</h3>
+          </div>
         </div>
       </div>
-    </div>
+    </label>
   </div>
 </template>
 
@@ -27,6 +32,7 @@ import { defineComponent, PropType } from "vue";
 import { weatherOptions } from "./types";
 export default defineComponent({
   name: "DateCard",
+  emits: ["date-clicked"],
   data: () => ({
     minTemp: "",
     maxTemp: "",
@@ -117,6 +123,9 @@ export default defineComponent({
         }
       });
       return this.turnInToDegree(avg / this.date.length);
+    },
+    selectDate() {
+      this.$emit("date-clicked", this.getDateDay());
     },
   },
   watch: {
