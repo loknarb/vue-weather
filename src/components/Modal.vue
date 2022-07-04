edf6ceeacfd0d560a68d20b9c544cac77b9db3ad
Sebastien Brumbaugh <66973931+loknarb@users.noreply.github.com>
2022-07-04 16:16:30 +0200

    applied slight changes and implemented date object and forlooped it

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -1,13 +1,47 @@
 <template>
-  <div><p>Hello we are in Modal vue</p></div>
+  <div>
+    <input type="checkbox" id="my-modal" class="modal-toggle" />
+    <label for="my-modal" class="modal cursor-pointer">
+      <label class="modal-box max-w-5xl relative" for="">
+        <div class="flex">
+          <div
+            class="bg-primary-focus opacity-90"
+            v-for="date in dateObjectDetail"
+            :value="date"
+            :key="date"
+          >
+            <p class="mx-2">{{ date.dt }}</p>
+            <!-- <figure><img :src="require(`../assets/${date.weather.icon}.@2x.png`)" /></figure> -->
+          </div>
+        </div>
+        <!-- <p class="text-2xl text-black">{{ dateObjectDetail }}</p> -->
+      </label>
+    </label>
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
+import { defineComponent, PropType } from "vue";
+import { weatherOptions } from "./types";
 export default defineComponent({
-  name: "App",
+  name: "Modal",
   components: {},
   data: () => ({}),
+  props: {
+    dateObjectDetail: {
+      type: Array as PropType<weatherOptions[]>,
+      required: true,
+    },
+    tempType: {
+      type: String as PropType<"F" | "C">,
+      required: true,
+    },
+  },
   methods: {},
+  watch: {
+    dateObjectDetail(newVal: weatherOptions[]) {
+      console.log(newVal);
+    },
+  },
 });
 </script>
