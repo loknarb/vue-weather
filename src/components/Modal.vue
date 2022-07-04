89fcff0a64d08d613e8322150c6cf3ebf98ec3e9
Sebastien Brumbaugh <66973931+loknarb@users.noreply.github.com>
2022-07-04 13:28:09 +0200

    modal init

---
+++ src/components/Modal.vue
@@ -0,0 +1,13 @@
+<template>
+  <div><p>Hello we are in Modal vue</p></div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+export default defineComponent({
+  name: "App",
+  components: {},
+  data: () => ({}),
+  methods: {},
+});
+</script>
