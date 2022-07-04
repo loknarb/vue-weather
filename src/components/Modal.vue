93e7cff691fbd903e0c5b5e4544907dbce664790
Sebastien Brumbaugh <66973931+loknarb@users.noreply.github.com>
2022-07-04 17:01:47 +0200

    mock-styling applied, added more data to each object, added quick epoch converter to show time in 24hr

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -3,15 +3,17 @@
     <input type="checkbox" id="my-modal" class="modal-toggle" />
     <label for="my-modal" class="modal cursor-pointer">
       <label class="modal-box max-w-5xl relative" for="">
-        <div class="flex">
+        <div class="flex w-full justify-between">
           <div
             class="bg-primary-focus opacity-90"
             v-for="date in dateObjectDetail"
             :value="date"
             :key="date"
           >
-            <p class="mx-2">{{ date.dt }}</p>
-            <!-- <figure><img :src="require(`../assets/${date.weather.icon}.@2x.png`)" /></figure> -->
+            <p class="mx-2">{{ convert(date.dt) }} HR</p>
+            <figure><img :src="require(`../assets/${date.weather.icon}@2x.png`)" /></figure>
+            <p class="mx-2">{{ date.main.fahrenheitTemp }}</p>
+            <p class="mx-2">{{ date.weather.description }}</p>
           </div>
         </div>
         <!-- <p class="text-2xl text-black">{{ dateObjectDetail }}</p> -->
@@ -37,10 +39,18 @@ export default defineComponent({
       required: true,
     },
   },
-  methods: {},
+  methods: {
+    convert(epochTime: number) {
+      const dt = new Date(epochTime * 1000);
+      const hr = dt.getUTCHours();
+      const m = "0" + dt.getUTCMinutes();
+
+      return hr + ":" + m.substring(-2);
+    },
+  },
   watch: {
     dateObjectDetail(newVal: weatherOptions[]) {
-      console.log(newVal);
+      // console.log(newVal);
     },
   },
 });
