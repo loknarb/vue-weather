2195123d601fc500ec85f8380b9f75c74788656e
Chiara Zitarosa <chiara.zitarosa@hotmail.com>
2022-07-04 18:56:49 +0200

    formatted tw styling, applied inline function for date

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -1,9 +1,9 @@
 <template>
   <div>
     <input type="checkbox" id="my-modal" class="modal-toggle" />
-    <label for="my-modal" class="modal cursor-pointer">
-      <label class="modal-box max-w-5xl relative" for="">
-        <div class="flex w-full justify-between">
+    <label for="my-modal" class="cursor-pointer modal">
+      <label class="relative max-w-5xl modal-box" for="">
+        <div class="flex justify-between w-full">
           <div
             class="bg-primary-focus opacity-90"
             v-for="date in dateObjectDetail"
@@ -12,7 +12,7 @@
           >
             <p class="mx-2">{{ convert(date.dt) }} HR</p>
             <figure><img :src="require(`../assets/${date.weather.icon}@2x.png`)" /></figure>
-            <p class="mx-2">{{ date.main.fahrenheitTemp }}</p>
+            <p class="mx-2">{{ tempTypeHandler(date.main) }}</p>
             <p class="mx-2">{{ date.weather.description }}</p>
           </div>
         </div>
@@ -28,7 +28,9 @@ import { weatherOptions } from "./types";
 export default defineComponent({
   name: "Modal",
   components: {},
-  data: () => ({}),
+  data: () => ({
+    selectedTemperatureType: "",
+  }),
   props: {
     dateObjectDetail: {
       type: Array as PropType<weatherOptions[]>,
@@ -40,6 +42,12 @@ export default defineComponent({
     },
   },
   methods: {
+    turnInToDegree(temp: string | number) {
+      if (typeof temp === "number") {
+        return temp.toFixed(0).toString() + "°";
+      }
+      return temp + "°";
+    },
     convert(epochTime: number) {
       const dt = new Date(epochTime * 1000);
       const hr = dt.getUTCHours();
@@ -47,11 +55,14 @@ export default defineComponent({
 
       return hr + ":" + m.substring(-2);
     },
-  },
-  watch: {
-    dateObjectDetail(newVal: weatherOptions[]) {
-      // console.log(newVal);
+    tempTypeHandler(main: weatherOptions["main"]) {
+      if (this.tempType === "F") {
+        return main.fahrenheitTemp;
+      } else {
+        return main.celsiusTemp;
+      }
     },
   },
+  watch: {},
 });
 </script>
